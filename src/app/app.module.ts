735ce9cfb83d6ddee115e6664cf70541e3e2f79a
Jairo Antonio Camarillo Tinoco <jcamarillo@lumston.com>
2024-01-19 00:45:24 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CharacterListComponent } from './components/character-list/character-list.component';
+import { CharacterDetailComponent } from './components/character-detail/character-detail.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterListComponent,
+    CharacterDetailComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule
